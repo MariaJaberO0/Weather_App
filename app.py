590a7d25d6ed36207c241df15e2833cd678f0308
Maria Jaber <maria.jaber.bit@gmail.com>
2026-06-30 13:28:20 +0300

    Initial version

---
+++ app.py
@@ -0,0 +1,17 @@
+print("hello")
+print(6*12)
+#my first app
+name =""
+print(name)
+a=10
+b=3
+#print("a+b =" + (a+b)) 
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+#print(a=b)
+print(a)
+print(10/6*6)
